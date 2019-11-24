deec28c3ed1947124cb3293398503243d21443c4
stnwtr <simon.steinkellner.ss@gmail.com>
2019-11-24 11:12:37 +0100

    priority file structure

--- src/main/java/at/stnwtr/indymatic/Indymatic.java
+++ src/main/java/at/stnwtr/indymatic/Indymatic.java
@@ -1,5 +1,9 @@
 package at.stnwtr.indymatic;
 
+import at.stnwtr.indy4j.credentials.Credentials;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Main class for this project.
  *
@@ -14,6 +18,17 @@ public class Indymatic {
    * @param args The command line arguments.
    */
   public static void main(String[] args) {
+    if (args.length != 3) {
+      System.out.println("Need 3 CommandLineArguments:");
+      System.out.println("  <username>         Indy username");
+      System.out.println("  <password>         Indy password");
+      System.out.println("  <priority_file>    The priority file");
+      System.exit(0);
+    }
+
+    Credentials credentials = Credentials.from(args[0], args[1]);
+    Path file = Paths.get(args[2]);
+
 
   }
 }
